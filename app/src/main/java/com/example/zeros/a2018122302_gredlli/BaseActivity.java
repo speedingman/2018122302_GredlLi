24a56fdd34667ccaa3e4e28a8fc6ec741505bf05
speedingman <hcdo@andats.com>
2018-12-23 13:41:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/zeros/a2018122302_gredlli/BaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.zeros.a2018122302_gredlli;
+
+import android.support.v7.app.AppCompatActivity;
+
+abstract class BaseActivity extends AppCompatActivity {
+
+    public abstract void bindViwes();
+
+    public abstract void setValue();
+
+    public abstract void setUpEvents();
+
+
+}
---
+++ app/src/main/java/com/example/zeros/a2018122302_gredlli/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.zeros.a2018122302_gredlli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViwes();
+        setUpEvents();
+        setValue();
+    }
+
+    @Override
+    public void bindViwes() {
+
+    }
+
+    @Override
+    public void setValue() {
+
+    }
+
+    @Override
+    public void setUpEvents() {
+
+    }
+}
